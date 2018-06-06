275cbcd9fdc62a1b7b50cb1e8524faaf94de5edb
bisht-100rav <bisht.100rav@gmail.com>
2018-06-06 21:52:30 +0530

    Assignment_2

---
+++ Assignment_2.py
@@ -0,0 +1,25 @@
+# ques_1
+print ("Hello python")
+
+# ques_2
+print ("Acad"+"View")
+
+# ques_3
+x=1
+y=2
+z=3
+print (x,y,z)
+
+#ques_4
+print ("Let\'s get started")
+
+#ques_5
+s="AcadView"
+course="Python"
+fees=5000
+print (" %s provides summer course in %s under the fees of %d"%(s,course,fees))
+
+#ques_6
+name="Tony Stark"
+salary=1000000
+print ("%s,%d"%(name,salary))
